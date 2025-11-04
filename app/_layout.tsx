c1c7e8a910c780ce198ac2a9ec1bdef4089c847f
appyouz <dikpalkhatri7@gmail.com>
2025-11-04 13:06:20 +0545

    refactor: move SafeAreaProvider to layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import './globals.css'
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  )
 }
