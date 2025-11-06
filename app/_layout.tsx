5bce8f9eb809827d69aa160fec60702a7360bb47
appyouz <dikpalkhatri7@gmail.com>
2025-11-06 12:26:09 +0545

    fix: add gesturehandlerview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,19 @@ import { Stack } from "expo-router";
 import './globals.css'
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TodosProvider } from './providers/TodosProvider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 export default function RootLayout() {
   return (
-
-    <SafeAreaProvider>
-      <TodosProvider>
-        <Stack
-          screenOptions={{ keepScreenOn: false }}
-        />
-      </TodosProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <TodosProvider>
+          <Stack
+            screenOptions={{ keepScreenOn: false }}
+          />
+        </TodosProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
 
   )
 }
